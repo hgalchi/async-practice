ffc684de283ec2a4cc89a1d9bba860ce91201fb1
SunMi <wywudi@naver.com>
2024-11-05 01:48:00 +0900

    feat : 초기설정

---
+++ src/main/java/practice/sync_asyncpractice/SyncAsyncPracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.sync_asyncpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncAsyncPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncAsyncPracticeApplication.class, args);
+	}
+
+}
